d9747b8f1f6b6ccc1323df4dc207a3a8d14bb6bb
pridon <fridontetradze@gmail.com>
2021-12-12 17:01:46 +0400

    async pipe instad of subscription in ts file

--- src/app/countries/countries-list/countries-list.component.ts
+++ src/app/countries/countries-list/countries-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
+import { Observable,  } from 'rxjs';
 import { CountryNameCode } from 'src/app/shared/models/country-name-code';
 import { CountriesListPageActions } from 'src/app/store/actions';
 import { AppSelectors } from 'src/app/store/selectors';
@@ -12,10 +12,8 @@ import { AppSelectors } from 'src/app/store/selectors';
   templateUrl: './countries-list.component.html',
   styleUrls: ['./countries-list.component.scss'],
 })
-export class CountriesListComponent implements OnInit, OnDestroy {
+export class CountriesListComponent implements OnInit {
   countries$: Observable<CountryNameCode[]> = this.store.select(AppSelectors.getCountriesList);
-  countriesSub = new Subscription();
-  countries: CountryNameCode[] = [];
   isLoading$: Observable<boolean> = this.store.select(AppSelectors.getIsLoading);
   searchText = '';
 
@@ -26,16 +24,10 @@ export class CountriesListComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.store.dispatch(CountriesListPageActions.loadPage());
-    this.countriesSub = this.countries$.subscribe(countryList => {
-      this.countries = countryList;
-    });
   }
 
   onCountryClicked(country: CountryNameCode) {
     this.router.navigate([`/countries/${country.countryCode}/${country.countryName}`]);
   }
 
-  ngOnDestroy(): void {
-    this.countriesSub.unsubscribe();
-  }
 }
